793bdd4770a4dcd411c2152da4f5fb6054ed9039
paumelia98 <paumelia98@gmail.com>
2024-03-05 18:09:27 +0100

    Actualizado el componente proxy

--- api/proxy.js
+++ api/proxy.js
@@ -1,24 +1,33 @@
 import fetch from 'node-fetch';
 
 export default async (req, res) => {
-    // La URL base y demás configuración...
     const baseUrl = 'http://api.football-data.org/v4/';
-    const path = req.url.replace('/api/', ''); // Ajusta según la estructura de tu solicitud
+    // Ajusta el path de la solicitud eliminando '/api/' del principio
+    const path = req.url.replace('/api/', '');
     const url = `${baseUrl}${path}`;
 
     const apiKey = '1a339e6dbc3b4c649cc23a710a7bb7fa';
+
+    // Define una función auxiliar para configurar los headers de CORS
+    const setCorsHeaders = (response) => {
+        response.setHeader('Access-Control-Allow-Origin', '*');
+        response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+    };
 
     try {
         const apiResponse = await fetch(url, {
             headers: { 'X-Auth-Token': apiKey },
         });
         const data = await apiResponse.json();
-        // Asegurándote de incluir los headers de CORS en la respuesta
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
+
+        // Configura los headers de CORS en la respuesta exitosa
+        setCorsHeaders(res);
         res.status(200).json(data);
     } catch (error) {
+        console.error(error);
+        // Asegúrate de configurar los headers de CORS también en las respuestas de error
+        setCorsHeaders(res);
         res.status(500).json({ error: 'Error al realizar la solicitud' });
     }
 };
