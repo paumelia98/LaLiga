842525630f97bf87442b1c67f7b183dfd783e110
paumelia98 <paumelia98@gmail.com>
2024-03-05 17:57:37 +0100

    Actualizado el componente MatchDay para usar el endpoint de Vercel

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,11 @@
-// api/proxy.js
 import fetch from 'node-fetch';
 
 export default async (req, res) => {
-    // La URL base de la API externa a la que te quieres conectar
+    // La URL base y demás configuración...
     const baseUrl = 'http://api.football-data.org/v4/';
-
-    // Adaptamos la lógica para construir la URL de solicitud
-    // Necesitarás ajustar esto para que funcione con la estructura de tu solicitud
-    const path = req.url.replace('/api/', ''); // Elimina el prefijo '/api/' de la URL
+    const path = req.url.replace('/api/', ''); // Ajusta según la estructura de tu solicitud
     const url = `${baseUrl}${path}`;
 
-    // Usar tu API key como antes
     const apiKey = '1a339e6dbc3b4c649cc23a710a7bb7fa';
 
     try {
@@ -18,6 +13,10 @@ export default async (req, res) => {
             headers: { 'X-Auth-Token': apiKey },
         });
         const data = await apiResponse.json();
+        // Asegurándote de incluir los headers de CORS en la respuesta
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept');
         res.status(200).json(data);
     } catch (error) {
         res.status(500).json({ error: 'Error al realizar la solicitud' });
