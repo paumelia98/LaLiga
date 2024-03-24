546d5fc990a1a2542f12f60b4b49512a98334eff
paumelia98 <paumelia98@gmail.com>
2024-03-24 20:21:21 +0100

    mejora accesibilidad

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # React + Vite + Tailwind
 
-Bienvenido a mi proyecto personal: una web educativa sobre la Liga de Fútbol Española. Este sitio, desarrollado con React y Tailwind CSS, ofrece una experiencia interactiva y educativa centrada en la pasión por el fútbol y el aprendizaje tecnológico. Visita el proyecto en https://la-liga-pau.vercel.app/.
+Bienvenido a un proyecto personal: una web educativa sobre la Liga de Fútbol Española. Este sitio, desarrollado con React y Tailwind CSS, ofrece una experiencia interactiva y educativa centrada en la pasión por el fútbol y el aprendizaje tecnológico. Visita el proyecto en https://la-liga-pau.vercel.app/.
 
 
--- src/components/ModalFirst.jsx
+++ src/components/ModalFirst.jsx
@@ -20,7 +20,7 @@ export const ModalFirst = ({ onClose }) => {
         
         <div className='flex flex-col justify-center items-center'>
           <p className='text-white text-xl lg:text-6xl font-bold lg:mb-12 my-4'>¡ATENCIÓN!</p>
-          <p className='font-semibold  text-xs lg:text-xl text-white'>Este sitio web ha sido desarrollado con fines educativos y opera gracias a una API gratuita. Para evitar superar el límite de solicitudes permitidas por minuto, te solicitamos amablemente evitar recargar la página innecesariamente.</p>
+          <p className='font-semibold  text-xs lg:text-xl text-white'>Este sitio web ha sido desarrollado con fines educativos y opera gracias a una API gratuita. Para evitar superar el límite de solicitudes, te solicitamos evitar recargar la página innecesariamente.</p>
           <button onClick={onClose} className='mt-4 bg-[#0b1121] text-white font-bold py-2 px-4 rounded transition-all hover:scale-105'>Entendido</button>
 
         </div>
